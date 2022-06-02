03620a2a970a73b49aaa7392653fefda899f67bc
musyokamuasya <josephmuasya8@gmail.com>
2022-06-03 02:07:47 +0300

    Add config, dependencies and versions files in buildSrc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        jcenter() // Warning: this repository is going to shut down soon
     }
 }
 rootProject.name = "Github"
-include ':presentation'
-include ':domain'
-include ':data'
+
+include("presentation", "domain", "data")
